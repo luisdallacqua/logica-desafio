66719cbd6d29c83b7830ff335a5f8946e2ca7434
luisdallacqua <luisdallacquaa@gmail.com>
2022-12-19 10:11:54 +0100

    finishing exercise 02 and 03

--- src/Exercicio02/Exercicio02.java
+++ src/Exercicio02/Exercicio02.java
@@ -1,4 +1,50 @@
 package Exercicio02;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio02 {
+    public static void main(String[] args) {
+        double dinheiroTotal = aceitaApenasDoublePositivoComoInput();
+
+        System.out.println("NOTAS:");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 100.00, "nota");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 50.00, "nota");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 20.00, "nota");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 10.00, "nota");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 5.00, "nota");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 2.00, "nota");
+        System.out.println("MOEDAS:");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 1.00, "moeda");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 0.50, "moeda");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 0.25, "moeda");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 0.10, "moeda");
+        dinheiroTotal = retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 0.05, "moeda");
+        retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(dinheiroTotal, 0.01, "moeda");
+    }
+
+    public static double retiradaDaQuantidadeDeUnidadeMonetariaEImpressaoNaTelaDaQuantidade(double dinheiro, double valor, String tipo){
+        int quantidadeDeDinheiro = (int) (dinheiro / valor);
+        dinheiro -= quantidadeDeDinheiro * valor;
+
+        System.out.printf(Locale.US, quantidadeDeDinheiro + "        " + tipo + "(s)         " + "    de      " + "R$  %.2f", valor);
+        System.out.println();
+        return dinheiro;
+    }
+
+    public static double aceitaApenasDoublePositivoComoInput(){
+        Scanner in = new Scanner(System.in);
+        double dinheiroTotal = -1;
+        while(dinheiroTotal < 0){
+            try{
+                in.useLocale(Locale.US);
+                dinheiroTotal = in.nextDouble();
+                if(dinheiroTotal < 0) throw new IllegalArgumentException("Não é possível adicionar número menor que 0");
+            }catch(IllegalArgumentException e){
+                e.printStackTrace();
+            }
+        }
+        return dinheiroTotal;
+    }
+
 }
--- src/Exercicio03/Exercicio03.java
+++ src/Exercicio03/Exercicio03.java
@@ -1,4 +1,36 @@
 package Exercicio03;
 
+import Exercicio01.Exercicio01;
+
+import java.util.HashMap;
+
 public class Exercicio03 {
+
+    public static void main(String[] args) {
+
+        int tamanhoDoArray = Exercicio01.aceitaApenasPositivosComoInput();
+        int valorAlvo = Exercicio01.aceitaApenasPositivosComoInput();
+
+        int[] arrayDeNumerosDoUsuario = new int[tamanhoDoArray];
+
+        for (int i = 0; i < tamanhoDoArray; i++){
+            int itemArray = Exercicio01.aceitaApenasPositivosComoInput();
+            arrayDeNumerosDoUsuario[i] = itemArray;
+        }
+
+        System.out.println(quantidadeDePares(arrayDeNumerosDoUsuario, valorAlvo));
+    }
+
+    static int quantidadeDePares(int[] arr, int valorAlvo){
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j< arr.length; j++){
+                if(arr[i] - arr[j] == valorAlvo || arr[j] - arr[i] == valorAlvo) count++;
+            }
+        }
+        return count;
+    }
+
+
+
 }
